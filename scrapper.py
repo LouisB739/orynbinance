8fcc9b85150a6621a06a688ae6d8b24f50190c5b
Louis <l2525b@gmail.com>
2019-10-20 16:59:00 +0200

    last

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,7 @@ import time
 from bs4 import BeautifulSoup
 import smtplib
 import gmail
+import whatsapp
 
 def binance_monitor():
 
@@ -17,13 +18,14 @@ def binance_monitor():
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         news = soup.find("li", class_="article-list-item")
-        
+
         time.sleep(15)
         
         if news == news2:
             print("....")
         else:
             gmail.send_mail("v.dalet@gmail.com","l2525b@gmail.com")
+            whatsapp.send_text()
             response2 = requests.get(url)
             soup2 = BeautifulSoup(response2.text, 'html.parser')
             news2 = soup2.find("li", class_="article-list-item")
